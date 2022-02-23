9373df24e14b8c6d4fac1fdf9532173fa85f7973
jean-on-hub <jean.eudes@amalitech.org>
2022-02-23 11:38:17 +0000

    project

---
+++ midtem.py
@@ -0,0 +1,9 @@
+import numpy as np
+# identify the probabilities
+# p(b) =" a student answered a question correctly"
+# p(a) ="she really knows the material?"
+# p_a = 0.6
+# p_b|a = 1- 0.15
+p_b = 0.85 *0.6 +0.2*0.4
+print(p_b)
+print(0.85 *0.6/0.59)
